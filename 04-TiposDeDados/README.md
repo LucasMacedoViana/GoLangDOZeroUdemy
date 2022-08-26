78191e64b188def428be0f8392abc0fefd67fd91
Lucas Macedo <lucasmacedo9@hotmail.com>
2022-08-26 13:47:53 -0300

    aula tipo de dados

--- 04-TiposDeDados/README.md
+++ 04-TiposDeDados/README.md
@@ -1,2 +1,41 @@
 ## Tipos de dados
 
+### Números
+inteiros e os reais ( int e o float)
+int 8, int16, int32, int64 - a diferença entre eles é o tamanho de bits que eles suportam
+
+quando vocce nao especifica o tamanho do int ele vai usar a arquitetura do seu computador como base.
+
+***uint*** - unsyned int é um int sem sinal (ou seja, somente os numeros postitivos)
+
+***rune*** - igual ao int32, numeros que representam caracteres
+
+***byte*** - igual ao uint8
+
+float32, float64 - suportao virgula, numeros quebrados
+
+Strings - texto, no go temos que colocar sempre em aspas duplas
+
+char - quando colocamos em aspas simples ele vai retornar o numero que aquele caracter representa na tabela ask e ele retorna um tipo int
+
+### Valor zero
+
+valor atribuido a uma variavel quando nao atribuimos um valor a ela
+
+### tipo boleano
+
+True ou False
+Valor zero é false
+
+### tipo Erro
+
+valor zero é nil
+
+error é o tipo da variavel
+
+errors é o tipo do pacote
+
+
+
+
+
--- 04-TiposDeDados/main.go
+++ 04-TiposDeDados/main.go
@@ -1,26 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main()  {
-	var variavel1 string = "Variavel 1"
-	variavel2 := "Variavel 2" // determinou o tipo da variavel pelo valor dela, inferencia de tipo (nome tecnico)
+	 var numero1 int16 = -100
+	 fmt.Println(numero1)
 
-	fmt.Println(variavel1)
-	fmt.Println(variavel2)
+	 var numero2 uint16 = 100
+	 fmt.Println(numero2)
 
-	var (
-		variavel3 string = "Variavel 3"
-		variavel4 string = "Variavel 4"
-		)
-	 fmt.Println(variavel3 , variavel4)
+	 //allias
+	 var numero3 rune = 12345 // igual ao int32, numeros que representam caracteres
+	 fmt.Println(numero3)
 
-	variavel5, variavel6 := "Variavel 5", "Variavel 6"
-	fmt.Println(variavel5, variavel6)
+	 var numero4 byte = 123 // igual ao uint8
+	 fmt.Println(numero4)
 
-	const constante1 string = "constate 1"
-	fmt.Println(constante1)
+	 var real1 float32= 123.45
+	 fmt.Println(real1)
 
-	variavel5, variavel6 = variavel6, variavel5 // invertendo as variaveis 
-	fmt.Println(variavel5, variavel6)
+	 var real2 float64 = 5671231231209.43
+	 fmt.Println(real2)
+
+	 real3 := 123123.32
+	 fmt.Println(real3)
+
+
+	 var str1 string = "texto 1"
+	 fmt.Println(str1)
+
+	str2 := "Texto 2"
+	fmt.Println(str2)
+
+	char := 'B'
+	fmt.Println(char)
+
+	var texto string // string vazia
+	var num int
+	fmt.Println(texto , num)
+
+	var booleano1 bool
+	fmt.Println(booleano1)
+
+	var erro1 error
+	fmt.Println(erro1)
+
+	var erro2 error = errors.New("erro interno")
+	fmt.Println(erro2)
+
 }
